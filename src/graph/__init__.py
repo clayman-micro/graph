6a0533037aad7e3151f653773f0e18151fe3b639
Kirill Sumorokov <sumorokov.k@gmail.com>
2021-12-16 22:53:33 +0500

    Add rabbitmq client

--- src/graph/__init__.py
+++ src/graph/__init__.py
@@ -1,22 +1,19 @@
+import aio_pika
 import structlog
 from fastapi import FastAPI
 from starlette_prometheus import PrometheusMiddleware, metrics
 
 from graph.logging import LoggingMiddleware
+from graph.settings import settings
 
 
 def init() -> FastAPI:
     """Initialize application."""
     app = FastAPI()
-    # app.state.settings = settings
+    app.state.settings = settings
 
-    @app.on_event("startup")
-    def on_startup():
-        app.logger.info("On startup")
-
-    @app.on_event("shutdown")
-    def on_shutdown():
-        app.logger.info("On shutdown")
+    # Configure rabbitmq
+    app.state.rabbitmq = None
 
     # Configure metrics
     app.add_middleware(PrometheusMiddleware)
@@ -25,5 +22,22 @@ def init() -> FastAPI:
     # Configure logging
     app.logger = structlog.get_logger("graph")  # type: ignore
     app.add_middleware(LoggingMiddleware)
+
+    @app.on_event("startup")
+    async def on_startup():
+        app.logger.info("On startup")
+
+        try:
+            app.state.rabbitmq = await aio_pika.connect(settings.rabbitmq_dsn)
+        except ConnectionError as exc:
+            app.logger.error("RabbitMQ connection error")
+
+            raise exc
+
+    @app.on_event("shutdown")
+    async def on_shutdown():
+        app.logger.info("On shutdown")
+
+        await app.state.rabbitmq.close()
 
     return app
--- src/graph/settings.py
+++ src/graph/settings.py
@@ -1,9 +1,17 @@
-from pydantic import BaseSettings
+from typing import Optional
+
+from pydantic import AnyUrl, BaseSettings, Field
+
+
+class RabbitMQDSN(AnyUrl):
+    allowed_schemes = {"amqp"}
+    user_required = True
 
 
 class Settings(BaseSettings):
-    sentry_dsn: str = ""
     debug: bool = False
+    rabbitmq_dsn: RabbitMQDSN = Field(default="amqp://rabbit:rabbit@localhost:5672/")
+    sentry_dsn: Optional[AnyUrl] = None
 
 
 settings = Settings()
